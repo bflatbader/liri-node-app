f696754badceb0e8f858193689c535fe4873ebb5
Bishop Bader <bflatbader@users.noreply.github.com>
2019-10-08 22:30:54 -0400

    Setup switch cases

--- liri.js
+++ liri.js
@@ -1,6 +1,30 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+var nodeSpotify = require('node-spotify-api');
+
+// Define keys for Spotify API
+var spotify = new nodeSpotify({
+    id: '3c99faab3e1843d395987fe86a73d1cc',
+    secret: '524a02087eb94d09ae1a27be7edcdfbb'
+});
 
 var command = process.argv[2];
 var args = process.argv;
+
+switch(command) {
+    case "concert-this":
+        // code
+        break;
+
+    case "spotify-this-song":
+        // code
+        break;
+
+    case "movie-this":
+        // code
+        break;
+
+    case "do-what-it-says":
+        // code
+        break;
+}
