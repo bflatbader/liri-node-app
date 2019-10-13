2db5af7134766672898bcbf5155bdde871cfc367
Bishop Bader <bflatbader@users.noreply.github.com>
2019-10-13 19:55:24 -0400

    Added movie-this function

--- liri.js
+++ liri.js
@@ -68,10 +68,10 @@ function spotify (song) {
             }
 
             console.log("\nTRACK".red);
-            console.log(response.tracks.items[0].album.name);
+            console.log(response.tracks.items[0].name);
 
             console.log("\nALBUM".red);
-            console.log(response.tracks.items[0].name);
+            console.log(response.tracks.items[0].album.name);
 
             console.log("\nPREVIEW".red);
             console.log(response.tracks.items[0].external_urls.spotify);
@@ -81,6 +81,48 @@ function spotify (song) {
         });
 }
 
+function movieThis (title) {
+    if (title) {
+        // A movie title was provided, do nothing
+    } else {
+        // No movie title was provided, default to "Mr. Nobody"
+        title = "mr nobody"
+    }
+    
+    omdbURL = "https://www.omdbapi.com/?t=" + encodeURIComponent(title) + "&plot=short&apikey=trilogy";
+
+    axios.get(omdbURL).then(
+        function(response) {
+                console.log("\nTITLE:        ".yellow + response.data.Title);
+                console.log("RELEASED:     ".yellow + response.data.Year);
+                console.log("IMDB RATING:  ".yellow + response.data.Ratings[0].Value);
+                console.log("TOMATOMETER:  ".yellow + response.data.Ratings[1].Value);
+                console.log("COUNTRY       ".yellow + response.data.Country);
+                console.log("LANGUAGE:     ".yellow + response.data.Language);
+                console.log("PLOT:         ".yellow + response.data.Plot);
+                console.log("ACTORS:       ".yellow + response.data.Actors);
+        })
+        .catch(function(error) {
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log("---------------Data---------------");
+              console.log(error.response.data);
+              console.log("---------------Status---------------");
+              console.log(error.response.status);
+              console.log("---------------Status---------------");
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an object that comes back with details pertaining to the error that occurred.
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });        
+}
 
 // VARIABLES
 var command = process.argv[2];
@@ -98,7 +140,7 @@ switch(command) {
         break;
 
     case "movie-this":
-        // code
+        movieThis(searchString);
         break;
 
     case "do-what-it-says":
